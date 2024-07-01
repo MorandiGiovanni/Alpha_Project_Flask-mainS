ff247d9c47695bdb57c86669dc5919c896ad769f
Giovanni Camargo Gardenal Morandi <162610567+MorandiGiovanni@users.noreply.github.com>
2024-07-01 16:37:35 -0300

    Add files via upload

---
+++ Alpha_Project_Flask-main (1)/Alpha_Project_Flask-main/app/controllers.py
@@ -0,0 +1,10 @@
+from app.models import Aluno, Professor
+
+alunos = [
+    Aluno("Aluno Teste", 23, "https://github.com/vitorrumke", "https://linkedin.com/in/vitorrumke", "Estudante de Engenharia.", "path/to/photo.jpg"),
+]
+
+professores = [
+    Professor("teste", "https://github.com/anacarolina", "https://linkedin.com/in/anacarolina", "Estudante de Engenharia.", "path/to/photo.jpg"),
+]
+ 
